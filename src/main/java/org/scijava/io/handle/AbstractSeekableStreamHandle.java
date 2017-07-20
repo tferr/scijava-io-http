9c0856d2fa9764fa2c5bedb1741149caa7dfa592
Curtis Rueden <ctrueden@wisc.edu>
2017-07-20 13:08:10 -0500

    Fix copyright and license headers

--- src/main/java/org/scijava/io/handle/AbstractSeekableStreamHandle.java
+++ src/main/java/org/scijava/io/handle/AbstractSeekableStreamHandle.java
@@ -1,10 +1,9 @@
 /*
  * #%L
- * SciJava Common shared library for SciJava software.
+ * SciJava I/O support for HTTP/HTTPS.
  * %%
- * Copyright (C) 2009 - 2017 Board of Regents of the University of
- * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
- * Institute of Molecular Cell Biology and Genetics.
+ * Copyright (C) 2017 KNIME GmbH and Board of Regents of the University
+ * of Wisconsin-Madison.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
